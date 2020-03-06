191f32d0fa981c1b2f76165ef6a5575365eab9a0
Patrick Pinto <patrick.cb3@gmail.com>
2020-03-06 10:39:41 +0000

    Convert case on response error

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 import { Service } from 'axios-middleware';
-import { snake } from 'change-case';
-import { camel } from './utils';
+import { snakeCase, camelCase } from 'change-case';
 import deepMapKeys from 'deep-map-keys';
 
 export const convertData = (data, method) => {
   let convertedData;
-
   if (typeof data === 'string') {
     convertedData = JSON.parse(data);
   } else {
@@ -16,17 +14,17 @@ export const convertData = (data, method) => {
   convertedData = deepMapKeys(convertedData, method);
 
   return convertedData;
-}
+};
 
 // Register a new service middleware to work with Rails backends, where typically the JSON keys are
 // sent as snake_case, that we want to convert to camelCase, since it's the default case in JS.
 export const registerService = () => {
-  const service = new Service(axios)
+  const service = new Service(axios);
 
   service.register({
     onRequest(request) {
       if (request.data) {
-        return { ...request, data: JSON.stringify(convertData(request.data, snake)) };
+        return { ...request, data: JSON.stringify(convertData(request.data, snakeCase)) };
       }
 
       return request;
@@ -36,12 +34,24 @@ export const registerService = () => {
     },
     onResponse(response) {
       if (response.data) {
-        return { ...response, data: convertData(response.data, camel) };
+        return { ...response, data: convertData(response.data, camelCase) };
       }
 
       return response;
+    },
+    onResponseError(error) {
+      const newError = error;
+
+      if (error.response && error.response.data) {
+        newError.response = {
+          ...error.response,
+          data: convertData(error.response.data, camelCase)
+        };
+      }
+
+      throw newError;
     }
-  })
-}
+  });
+};
 
 export default registerService;
